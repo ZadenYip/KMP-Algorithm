f7bd96fd5b971702352563a6fba6e9202e4e8e1a
HyFran <hyfrancn@gmail.com>
2024-10-19 12:21:17 +0800

    refactor: keep the code close to the state machine concept

--- C++/KMP/main.cpp
+++ C++/KMP/main.cpp
@@ -1,40 +1,76 @@
 #include <iostream>
 #include <cstring>
+#include <exception>
 
 using std::string;
 
-int* BuildPMT(string pattern) {
-    int patternLength = pattern.length();
-    int* pmt = new int[patternLength];
-    pmt[0] = 0;
-    int currentMaxLength = 0;
-    for (int i = 1; i < patternLength; i++) {
-        while (currentMaxLength > 0 && pattern.at(currentMaxLength) != pattern.at(i)) {
-            currentMaxLength = pmt[currentMaxLength - 1];
-        }
-        if (pattern.at(currentMaxLength) == pattern.at(i)) {
-            currentMaxLength++;
-        }
-        pmt[i] = currentMaxLength;
+int* BuildMoveState(string pattern) {
+    if (pattern.length() == 0)
+    {
+        throw std::invalid_argument("字符串长度不能为0");
     }
-    return pmt;
+    int pattern_length = pattern.length();
+    int move_state_length = pattern_length + 1;
+    //moveState状态机数组比pattern长度是多一个的，最后一个状态表示终止状态
+    int* move_state = new int[move_state_length];
+    int revert_state = 0;
+    /*
+    状态机0和1肯定是没有公共前后缀, 所以回溯肯定是回到状态0
+     */
+    move_state[0] = 0;
+    move_state[1] = 0;
+
+    int state = 2;
+    while (state < move_state_length) {
+        int suffixIndex = state - 1;
+        //pattern.charAt(revertState)为到达下一个状态需要的字符
+        if (pattern.at(suffixIndex) == pattern.at(revert_state)) {
+            revert_state++;
+            move_state[state] = revert_state;
+            state++;
+        }
+        else {
+            if (revert_state > 0) {
+                revert_state = move_state[revert_state];
+                //这里不进入下一阶段是因为回溯了 此时要对比suffixIndex与进入下一个状态所需的字符是否一样
+            }
+            else {
+                //此时已经是0状态 推进state即可
+                state++;
+            }
+        }
+    }
+    return move_state;
 }
 
 int Search(string text, string pattern) {
-    int* pmt = BuildPMT(pattern);
-    for (int i = 0, j = 0; i < text.length() && j < pattern.length(); i++) {
-        while (j > 0 && text.at(i) != pattern.at(j)) {
-            j = pmt[j - 1];
+    int* move_state = BuildMoveState(pattern);
+    int move_state_length = pattern.length() + 1;
+    /* 不妨把i 作为每次text作为一个队列弹出的第一个元素的pop为输入给状态机判断是否进入下一个状态 */
+    int input = 0;
+    int state = 0;
+    while (input < text.length()) {
+        //pattern.charAt(state)为到达下一个状态需要的字符
+        if (text.at(input) == pattern.at(state)) {
+            state++;
+            input++;
+            if (state == move_state_length - 1) {
+                delete [] move_state;
+                return input - pattern.length();
+            }
         }
-        if (text.at(i) == pattern.at(j)) {
-            j++;
-        }
-        if (j == pattern.length()) {
-            delete [] pmt;
-            return i - j + 1;
+        else {
+            if (state > 0) {
+                state = move_state[state];
+                //这里不进行input++, 因为回溯后要查看输入等于回溯后进入下一个状态的条件
+            }
+            else {
+                //零状态直接推进
+                input++;
+            }
         }
     }
-    delete [] pmt;
+    delete [] move_state;
     return -1;
 }
 
